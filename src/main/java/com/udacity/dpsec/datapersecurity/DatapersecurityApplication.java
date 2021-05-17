624dde5d82929a954e41290ad58233d12d5c2fdf
Haşim <hasimseo@gmail.com>
2021-05-17 20:05:20 +0300

    Initial commit

---
+++ src/main/java/com/udacity/dpsec/datapersecurity/DatapersecurityApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.dpsec.datapersecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatapersecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatapersecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udacity/dpsec/datapersecurity/DatapersecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.dpsec.datapersecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatapersecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
