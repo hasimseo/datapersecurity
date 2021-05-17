c141a0ac6c0429acf7b32ef2f2d87dc15dcf3e4a
Haşim <hasimseo@gmail.com>
2021-05-18 02:24:20 +0300

    final

---
+++ src/main/java/com/udacity/dpsec/datapersecurity/service/MessageService.java
@@ -0,0 +1,44 @@
+package com.udacity.dpsec.datapersecurity.service;
+
+import com.udacity.dpsec.datapersecurity.mapper.MessageMapper;
+import com.udacity.dpsec.datapersecurity.model.ChatForm;
+import com.udacity.dpsec.datapersecurity.model.ChatMessage;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class MessageService {
+
+    private MessageMapper messageMapper;
+
+    public MessageService(MessageMapper messageMapper) {
+        this.messageMapper = messageMapper;
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("Createing MessageService Bean");
+    }
+
+    public void addMessage(ChatForm chatForm){
+        ChatMessage newMessage = new ChatMessage();
+        newMessage.setUsername(chatForm.getUsername());
+        switch (chatForm.getMessageType()){
+            case "Say":
+                newMessage.setMessageText(chatForm.getMessageText());
+                break;
+            case "Shout":
+                newMessage.setMessageText(chatForm.getMessageText().toUpperCase());
+                break;
+            case "Whisper":
+                newMessage.setMessageText(chatForm.getMessageText().toLowerCase());
+                break;
+        }
+        messageMapper.addMessage(newMessage);
+    }
+
+    public List<ChatMessage> getChatMessages(){ return messageMapper.getAllMessages();}
+
+}
